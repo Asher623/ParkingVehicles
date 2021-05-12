83fc25a2b459fa83c1f9a0b2a9b285c720507ff9
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:37:36 -0400

    Update Motorcycle.cpp

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -1,18 +1,4 @@
-/* Citation and Sources...
-Final Project Milestone 5
-Module: Motorcycle
-Filename: Motorcycle.cpp
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/30 Preliminary release
-2020/07/30 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
+
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
