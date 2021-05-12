71757a9a7b060d6f97ad074649f07f7b08dfdc99
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:40:01 -0400

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,18 +1,4 @@
-/* Citation and Sources...
-Final Project Milestone 4
-Module: Vehicle
-Filename: Vehicle.h
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/08 Preliminary release
-2020/07/23 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
+
 #ifndef SDDS_VEHICLE_H
 #define SDDS_VEHICLE_H
 #include <iostream>
