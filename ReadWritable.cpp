f34b6d7ae7334a8584d2c703a0f831ed3688a4ec
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:38:52 -0400

    Update ReadWritable.cpp

--- ReadWritable.cpp
+++ ReadWritable.cpp
@@ -1,18 +1,4 @@
-/* Citation and Sources...
-Final Project Milestone 3
-Module: ReadWritable
-Filename: ReadWritable.h
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/08 Preliminary release
-2020/07/22 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string.h>
