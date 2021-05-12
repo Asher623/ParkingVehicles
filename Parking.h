55f9b04e329fd611ef8310b32d16565b628d91b8
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:38:16 -0400

    Update Parking.h

--- Parking.h
+++ Parking.h
@@ -1,18 +1,4 @@
-/* Citation and Sources...
-Final Project Milestone 6
-Module: Menu
-Filename: Menu.h
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/08 Preliminary release
-2020/07/08 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
+
 
 #ifndef SDDS_PARKING_H
 #define SDDS_PARKING_H
