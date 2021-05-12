735dc89f18c581bab5eb97f9834cfea71543e55b
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:39:04 -0400

    Update ReadWritable.h

--- ReadWritable.h
+++ ReadWritable.h
@@ -1,18 +1,3 @@
-/* Citation and Sources...
-Final Project Milestone 3
-Module: ReadWritable
-Filename: ReadWritable.h
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/08 Preliminary release
-2020/07/22 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
 
 #ifndef SDDS_MENU_H
 #define SDDS_MENU_H
