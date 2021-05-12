e5d345c781985593b0a42e47866838ff79b17410
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-12 10:37:46 -0400

    Update Motorcycle.h

--- Motorcycle.h
+++ Motorcycle.h
@@ -1,18 +1,4 @@
-/* Citation and Sources...
-Final Project Milestone 5
-Module: Motorcycle
-Filename: Motorcycle.h
-Version 1.0
-Author Ivan Anferov
-Revision History
------------------------------------------------------------
-Date Reason
-2020/07/30 Preliminary release
-2020/07/30 Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
+
 #ifndef SDDS_MOTORCYCLE_H
 #define SDDS_MOTORCYCLE_H
 #include <iostream>
